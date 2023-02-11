8e329427194c471f840b4301703a3901aa77410f
Akshaya2807 <115683252+Akshaya2807@users.noreply.github.com>
2023-02-11 18:20:16 +0530

    Create Charging.h

---
+++ Charging.h
@@ -0,0 +1,11 @@
+#include "stdio.h"
+
+int number_of_readings = 10;
+int count_Range1=0;
+int count_Range2=0;
+int currentReadings_a[] = {3,5,4,10,6,7,8,9,3,5};
+
+void GetRangeOfReading();
+void GetRangeOfReading();
+int CalculateRange1();
+int CalculateRange2();
