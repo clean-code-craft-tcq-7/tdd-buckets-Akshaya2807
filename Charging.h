4d96eef8e60febed68d04ba4222a8ba87ba8a3b1
Akshaya2807 <115683252+Akshaya2807@users.noreply.github.com>
2023-02-24 11:14:17 +0530

    Update Charging.h

--- Charging.h
+++ Charging.h
@@ -1,11 +1,4 @@
-#include "stdio.h"
+#include <stdio.h>
 
-int number_of_readings = 10;
-int count_Range1=0;
-int count_Range2=0;
-int currentReadings_a[] = {3,5,4,10,6,7,8,9,3,5};
-
-void GetRangeOfReading();
-void GetRangeOfReading();
-int CalculateRange1();
-int CalculateRange2();
+int getNoOfRValuesInRange(int* array_p,int startRange ,int endRange);
+bool checkInRange(int val,int startRange ,int endRange);
