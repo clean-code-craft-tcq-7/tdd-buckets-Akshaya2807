44d3fcb2a70ce381c5b3f49f055209691db90ac1
Akshaya2807 <115683252+Akshaya2807@users.noreply.github.com>
2023-02-24 11:13:14 +0530

    Update Charging.c

--- Charging.c
+++ Charging.c
@@ -1,45 +1,35 @@
-#include "Charging.h"
 #include <stdio.h>
+#include "Charging.h"
 
-int CalculateRange1(int currentRed_a[])
+bool checkInRange(int val,int startRange ,int endRange)
 {
-  for(i=0;i<number_of_readings;i++)
+  if((val >= startRange) && (val <= endRange))
   {
-    CountRange_One(currentRed_a);
+    return 1;
   }
-  return count_Range1;
-}
-int CalculateRange2(int currentRed_a[])
-{
-  for(i=0;i<number_of_readings;i++)
+  else
   {
-    CountRange_Two(currentRed_a);
-  }
-  return count_Range2;
-}
-int CountRange_One(int current_a[])
-{
-  
-  if((current_a[i] >=3) && (current_a[i] <6))
-  {
-    count_Range1 +=1;
-  }
-  return count_Range1;
-}
-int CountRange_Two(int current_a[])
-{
-  if((current_a[i]<=6) && (current_a[i] <=10))
-  {
-    count_Range2 +=1;
-  }
-  return count_Range1;
-}
-int main()
-{
-    CalculateRange1(currentReadings_a);
-    CalculateRange2(currentReadings_a);
-    printf("Range,\treadings");
-    printf("\n3-5,\t%d",count_Range1);
-    printf("\n6-10,\t%d",count_Range2);
     return 0;
+  }
 }
+
+int getNoOfRValuesInRange(int* array_p,int startRange ,int endRange)
+{
+  int arrayLength =0;
+  int valueInRange =0;
+  arrayLength = sizeof(array_p);
+  for(int i=0; i< arrayLength ;i++)
+  {
+       if(true == checkInRange(array_p[i],startRange,endRange))
+       {
+         valueInRange ++;
+       }
+       else
+       {
+         
+       }
+  }
+  printf("\n%d-%d,%d",startRange,endRange,valueInRange);
+  return valueInRange;
+}
+
